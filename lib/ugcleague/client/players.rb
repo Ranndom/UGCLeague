259351adf711590c94c4b2b4919771f9e60a7d5b
Ranndom <ranndom@rnndm.xyz>
2015-09-26 02:03:21 +1000

    Added player_has_profile endpoint.

--- lib/ugcleague/client/players.rb
+++ lib/ugcleague/client/players.rb
@@ -1,6 +1,10 @@
 class UGC::Client
     module Players
 
+        def player_has_profile(steamid)
+            get({:exists => steamid})
+        end
+
         def player_history(steamid)
             get({:player => steamid})
         end
